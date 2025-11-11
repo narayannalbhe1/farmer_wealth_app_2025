ebc55afe5fe1be5c313ea1e4e3808985732d6879
narayannalbhe1 <nalbhenarayan1@gmail.com>
2025-11-11 22:41:04 +0530

    2nd commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:farmerwealthapp/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const FarmerWealthApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
